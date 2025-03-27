626ad7fbda8375e796fd5d20f966c71e970c1e8c
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2025-03-27 18:42:54 +0100

    Add unit tests for GameManager methods

--- src/test/java/io/github/pseudo/GameManagerTest.java
+++ src/test/java/io/github/pseudo/GameManagerTest.java
@@ -10,6 +10,26 @@ class GameManagerTest {
         int nbNoeuds = 5;
         GameManager gameManager = new GameManager(nbNoeuds);
 
-        assertNotNull(gameManager);
+        assertEquals(gameManager.getNode(0).getIndex(), 0);
+    }
+
+    @Test
+    void testAddLink() {
+        int nbNoeuds = 5;
+        GameManager gameManager = new GameManager(nbNoeuds);
+        gameManager.addLink(1, 2);
+
+        assertEquals(gameManager.getNode(1).getLinks().size(), 1);
+        assertEquals(gameManager.getNode(2).getLinks().size(), 1);
+    }
+
+    @Test
+    void testSetAsPaserelle() {
+        int nbNoeuds = 5;
+        GameManager gameManager = new GameManager(nbNoeuds);
+        gameManager.addLink(0, 1);
+        gameManager.setAsPaserelle(1);
+
+        assertTrue(gameManager.getNode(1).isPaserelle());
     }
 }
